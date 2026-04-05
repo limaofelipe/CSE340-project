ab2267dc8bad155e8fa526afce4f9dd0867b4e6a
limaofelipe <dev.felipelima@gmail.com>
2026-04-05 19:36:16 -0300

    update v2

--- server.js
+++ server.js
@@ -2,18 +2,61 @@
  * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
+// Their stuff
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config();
-const app = express();
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+// My stuff
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute.js");
+const accountRoute = require('./routes/accountRoute.js');
+const messageRoute = require('./routes/messageRoute.js');
 const intentionalErrorRoute = require("./routes/intentionalErrorRoute.js");
 const utilities = require("./utilities/index.js");
+const pool = require("./database");
+
+// Init
+const app = express();
+const env = require("dotenv").config();
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+);
+// Express Messages Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ // for parsing application/x-www-form-urlencoded
+  extended: true
+}));
+// Cookie parser
+app.use(cookieParser())
+// JWT checker
+app.use(utilities.checkJWTToken);
 
 /* ***********************
  * View Engine and Templates
@@ -22,8 +65,6 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // Not at view root
 
-
-app.use(express.static("public"))
 /* ***********************
  * Routes
  *************************/
@@ -32,14 +73,16 @@ app.use(static);
 app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use("/inv", inventoryRoute);
+// Account routes
+app.use("/account", accountRoute);
+// Message routes
+app.use("/message", messageRoute);
 // Intentional error route. Used for testing
 app.use("/ierror", intentionalErrorRoute);
-
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Unfortunately, we don\'t have that page in stock.'})
 })
-
 
 /* ***********************
 * Express Error Handler
@@ -56,7 +99,6 @@ app.use(async (err, req, res, next) => {
     nav
   })
 })
-
 
 /* ***********************
  * Local Server Information
